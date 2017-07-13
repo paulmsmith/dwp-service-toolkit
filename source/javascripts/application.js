0656f705a6f2171d9a19de3fa506faac6856c920
Paul Smith <mail@paulsmith.site>
2017-07-13 13:03:49 +0100

    initial dwp content styleguide as part of the prototype dwp service toolkit

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -39,4 +39,54 @@ $(function() {
 
   });
 
+  var docs = {};
+
+  docs.accordion = {
+
+    config: {
+      selector: ".js-accordion",
+      itemSel: ".js-accordion-item",
+      toggleSel: ".js-accordion-item-toggler",
+      contentSel: ".js-accordion-item-content",
+      hideClass: "js-hide",
+      activeClass: 'js-active'
+    },
+
+    makeAccordion: function makeAccordion($accordion) {
+      var $items = $accordion.find(this.config.itemSel);
+      if(!!$items.length) {
+        $items.each(function(index, item){
+          var $currentItem = $(item),
+            $toggler = $currentItem.find(this.config.toggleSel),
+            $content = $currentItem.find(this.config.contentSel);
+          $toggler.on('click', function(e){
+            $toggler.toggleClass(this.config.activeClass);
+            $content.toggleClass(this.config.hideClass);
+          }.bind(this))
+
+        }.bind(this));
+      }
+    },
+
+    init: function init() {
+      var $accordions = $(this.config.selector);
+      if($accordions.length > 0) {
+        $accordions.each(function(index, currentAccordion){
+          this.makeAccordion($(currentAccordion));
+        }.bind(this));
+      }
+    }
+
+  }
+
+  /**
+   * init stuff
+   * @method init
+   */
+  docs.init = function() {
+    docs.accordion.init();
+  }
+
+  docs.init();
+
 });
